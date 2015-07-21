92963655b0fd403740f6511720e91aa9cec85cb1
Loller79 <loller790@gmail.com>
2015-07-21 14:48:03 +0200

    PM: Introduce suspend state PM_SUSPEND_FREEZE
    Conflicts:
    	kernel/power/suspend.c
    
    Conflicts:
    	drivers/base/power/wakeup.c

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -399,6 +399,12 @@ static void wakeup_source_activate(struct wakeup_source *ws)
 	if (!enable_wlan_wake_ws && !strcmp(ws->name, "wlan_wake"))
                 return;
 
+	/*
+	 * active wakeup source should bring the system
+	 * out of PM_SUSPEND_FREEZE state
+	 */
+	freeze_wake();
+
 	ws->active = true;
 	ws->active_count++;
 	ws->last_time = ktime_get();
