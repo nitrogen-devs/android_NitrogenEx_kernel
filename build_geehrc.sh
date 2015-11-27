681e9d98f6620e239863c19e8fdd25e36236ce7f
Mr.MEX <binder_z@mail.ru>
2015-11-27 23:12:22 +0400

    Build scripts

---
+++ build_geehrc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export ARCH=arm
+export CROSS_COMPILE=../Linaro-4.9.4/bin/arm-eabi-
+export CCACHE_DIR=~/.ccache/nitrogenex
+export KBUILD_BUILD_USER=mr_mex
+export KBUILD_BUILD_HOST=hp_pc
+make gee_defconfig
+make -j 8 -o4
