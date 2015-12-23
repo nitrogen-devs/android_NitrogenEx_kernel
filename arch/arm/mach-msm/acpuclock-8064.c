43cde9df8aa363369568d7bb7fb4aef6046639da
Mr.MEX <binder_z@mail.ru>
2015-12-23 13:47:50 +0400

    CPU: Fix voltage for 162 MHz

--- arch/arm/mach-msm/acpuclock-8064.c
+++ arch/arm/mach-msm/acpuclock-8064.c
@@ -138,7 +138,7 @@ static struct l2_level l2_freq_tbl[] __initdata = {
 };
 
 static struct acpu_level tbl_slow[] __initdata = {
-    { 1, {   162000, HFPLL, 0, 0x0C }, L2(0),   950000 }, 
+    { 1, {   162000, HFPLL, 0, 0x0C }, L2(0),   900000 }, 
 	{ 1, {   192000, PLL_8, 0, 0x0C }, L2(0),   925000 },
 	{ 1, {   270000, HFPLL, 0, 0x14 }, L2(0),   950000 },
 	{ 1, {   384000, PLL_8, 0, 0x00 }, L2(0),   950000 },
@@ -169,7 +169,7 @@ static struct acpu_level tbl_slow[] __initdata = {
 };
 
 static struct acpu_level tbl_nom[] __initdata = {
-	{ 1, {   162000, HFPLL, 0, 0x0C }, L2(0),   900000 }, 
+	{ 1, {   162000, HFPLL, 0, 0x0C }, L2(0),   850000 }, 
 	{ 1, {   192000, PLL_8, 0, 0x0C }, L2(0),   875000 },
 	{ 1, {   270000, HFPLL, 0, 0x14 }, L2(0),   900000 },
 	{ 1, {   384000, PLL_8, 0, 0x00 }, L2(0),   900000 },
@@ -200,7 +200,7 @@ static struct acpu_level tbl_nom[] __initdata = {
 };
 
 static struct acpu_level tbl_fast[] __initdata = {
-	{ 1, {   162000, HFPLL, 0, 0x0C }, L2(0),   850000 }, 
+	{ 1, {   162000, HFPLL, 0, 0x0C }, L2(0),   800000 }, 
 	{ 1, {   192000, PLL_8, 0, 0x0C }, L2(0),   825000 },
 	{ 1, {   270000, HFPLL, 0, 0x14 }, L2(0),   850000 }, 
 	{ 1, {   384000, PLL_8, 0, 0x00 }, L2(0),   850000 },
@@ -231,7 +231,7 @@ static struct acpu_level tbl_fast[] __initdata = {
 };
 
 static struct acpu_level tbl_faster[] __initdata = {
-	{ 1, {   162000, HFPLL, 0, 0x0C }, L2(0),   850000 }, 
+	{ 1, {   162000, HFPLL, 0, 0x0C }, L2(0),   800000 }, 
 	{ 1, {   192000, PLL_8, 0, 0x0C }, L2(0),   825000 },
 	{ 1, {   270000, HFPLL, 0, 0x14 }, L2(0),   850000 },
 	{ 1, {   384000, PLL_8, 0, 0x00 }, L2(0),   850000 },
