480e0ed0e8bdb919b94a91b40e7ee7f407a140b1
Mr-MEX <binder_z@mail.ru>
2016-01-29 00:12:41 +0400

    Revert "Battery Life eXtender (BLX)"
    This reverts commit 73aa98ad9ee1054717cd864efea652aacfd48caf.

--- drivers/power/pm8921-charger.c
+++ drivers/power/pm8921-charger.c
@@ -27,7 +27,6 @@
 #include <linux/workqueue.h>
 #include <linux/debugfs.h>
 #include <linux/slab.h>
-#include <linux/blx.h>
 #include <linux/mfd/pm8xxx/batt-alarm.h>
 
 #include <mach/msm_xo.h>
@@ -1567,18 +1566,14 @@ static int get_prop_batt_status(struct pm8921_chg_chip *chip)
 	}
 
 	if (chip->eoc_check_soc) {
-    #ifdef CONFIG_BLX
-        if (get_prop_batt_capacity(chip) >= get_charginglimit())
-    #else
-            if (get_prop_batt_capacity(chip) == 100) 
-    #endif
+		if (get_prop_batt_capacity(chip) == 100) {
 			if (batt_state == POWER_SUPPLY_STATUS_CHARGING)
 				batt_state = POWER_SUPPLY_STATUS_FULL;
-    }   else {
+		} else {
 			if (batt_state == POWER_SUPPLY_STATUS_FULL)
 				batt_state = POWER_SUPPLY_STATUS_CHARGING;
 		}
-	
+	}
 
 	pr_debug("batt_state = %d fsm_state = %d \n",batt_state, fsm_state);
 	return batt_state;
@@ -3438,11 +3433,7 @@ static void eoc_worker(struct work_struct *work)
 
 	if (chip->eoc_check_soc) {
 		percent_soc = get_prop_batt_capacity(chip);
-    #ifdef CONFIG_BLX
-        if (percent_soc >= get_charginglimit())
-    #else
-            if (percent_soc == 100)
-    #endif
+		if (percent_soc == 100)
 			count = CONSECUTIVE_COUNT;
 	}
 
@@ -3454,18 +3445,11 @@ static void eoc_worker(struct work_struct *work)
 
 		if (is_ext_charging(chip))
 			chip->ext_charge_done = true;
-    #ifdef CONFIG_BLX
-        //if (chip->is_bat_warm || chip->is_bat_cool)
-          //  chip->bms_notify.is_battery_full = 0;
-        //else
-          //  chip->bms_notify.is_battery_full = 1;
-    #else
-        if (chip->is_bat_warm || chip->is_bat_cool)
-          chip->bms_notify.is_battery_full = 0;
-        else
-          chip->bms_notify.is_battery_full = 1;
-     #endif
-        
+
+		if (chip->is_bat_warm || chip->is_bat_cool)
+			chip->bms_notify.is_battery_full = 0;
+		else
+			chip->bms_notify.is_battery_full = 1;
 		/* declare end of charging by invoking chgdone interrupt */
 		chgdone_irq_handler(chip->pmic_chg_irq[CHGDONE_IRQ], chip);
 		wake_unlock(&chip->eoc_wake_lock);
