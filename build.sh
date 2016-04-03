56e547f2a9e92c1f5cf3bd4c0556ed31401521c0
Mr-MEX <binder_z@mail.ru>
2016-04-03 13:44:13 +0400

    Change build user, build host

--- build.sh
+++ build.sh
@@ -19,8 +19,8 @@ VER=".2.7"
 NEX_VER="$BASE_NEX_VER$VER"
 
 # Vars
-export KBUILD_BUILD_USER=builder
-export KBUILD_BUILD_HOST=nitrogen-build
+export KBUILD_BUILD_USER="Mr.MEX"
+export KBUILD_BUILD_HOST="nitrogen-pc"
 export CCACHE_DIR=~/.ccache/nitrogenex
 export LOCALVERSION=.`echo $NEX_VER`
 export ARCH=arm
